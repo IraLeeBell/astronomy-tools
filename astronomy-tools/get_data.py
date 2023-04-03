4b5fbb3d19dd1b117e7162a8208110cca823d1f7
Ira Bell <ira@irabell.com>
2023-04-02 23:12:47 -0500

    Use numpy to grab the pixel values, then be able to specify the number of rows and columns we want. So my test image is 1994 x 2405. That means that the image is 4.7 million pixels. I believe if we set num_rows and num_columns to 1, we should get the pixel value for the first pixel in the image (top left corner)

--- astronomy-tools/get_data.py
+++ astronomy-tools/get_data.py
@@ -3,20 +3,29 @@
 # Originaly written by Ira Bell
 # -=-=-=--=-=-=-=-=-=-=-=-=--=-
 
+import numpy as np
 from astropy.io import fits
 
 # Open the FITS file
 hdul = fits.open('hlsp_jwst-ero_jwst_miri_stephansquintet_f1000w_v1_i2d.fits')
 
-# Get the pixel data from the first HDU
+# Get the pixel data
 data = hdul[0].data
 
-# Print the shape of the data array
+# Print the shape of the data
 print("Data shape:", data.shape)
 
-# Print the first few elements of the data array
+# Set the numpy print options
+np.set_printoptions(threshold=np.inf, precision=4, suppress=True)
+
+# Set the number of rows to print
+num_rows = 1
+num_columns = 1
+
+# Print the data
 print("Data values:")
-print(data[:10, :10])
+print(data[:num_rows, :num_columns:])
 
 # Close the FITS file
 hdul.close()
+
