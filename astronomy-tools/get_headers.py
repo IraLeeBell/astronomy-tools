71c5e5f7b0d38a01815ef3dec4561a72874a604e
Ira Bell <ira@irabell.com>
2023-04-02 21:48:48 -0500

    Update get_headers.py
    Trying to deal with the COMMENTS header and the fact that it (1) possibly has blanks and (2) that its more than one header in a row with the same name. Goal is to first print out all of the headers from a FITS file. Then go through a process where we assign each header we find to a variable. Then we should print all of the contents of each variable out to see what was created in terms of variable names and their assigned values.

--- astronomy-tools/get_headers.py
+++ astronomy-tools/get_headers.py
@@ -3,17 +3,36 @@ from astropy.io import fits
 # Open the FITS file
 hdul = fits.open('hlsp_jwst-ero_jwst_miri_stephansquintet_f1000w_v1_i2d.fits')
 
+
 # Get the header of the primary HDU
 header = hdul[0].header
 
+header_keys = list(header.keys())
+print(header_keys)
+
 # Create variables for each header keyword and assign its value
 for key in header:
-    exec(f"{key.lower()} = header['{key}']")
+    if key == 'COMMENT':
+        if isinstance(header[key], str) and header[key].strip():
+            value = header[key].replace("'", "\\'").replace('"', '\\"')
+            exec(f"{key.lower().replace('-', '_')} = '{value}'")
+    elif key == 'CONTINUE':
+        continue
+    else:
+        var_name = key.lower().replace('-', '_')
+        try:
+            if isinstance(header[key], str):
+                value = header[key].replace("'", "\\'").replace('"', '\\"')
+                exec(f"{var_name} = '{value}'")
+            else:
+                exec(f"{var_name} = {header[key]}")
+        except fits.verify.VerifyError as e:
+            if str(e).startswith('CONTINUE cards must have string values'):
+                continue
+            else:
+                raise e
 
-# Print the values of the variables
-print("Location name:", location)
-print("Exposure time:", exptime)
-print("Filter name:", filter)
+
 
 # Close the FITS file
 hdul.close()
