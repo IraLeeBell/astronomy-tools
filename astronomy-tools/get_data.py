065698e1bba2af8c4507ffd12941fc273ff02193
Ira Bell <ira@irabell.com>
2023-04-04 20:34:49 -0500

    Update get_data.py

--- astronomy-tools/get_data.py
+++ astronomy-tools/get_data.py
@@ -1,26 +1,26 @@
 # -=-=-=--=-=-=-=-=-=-=-=-=--=-
 # MIT License
-# Originaly written by Ira Bell
+# Originally written by Ira Bell
 # -=-=-=--=-=-=-=-=-=-=-=-=--=-
 
 import numpy as np
 from astropy.io import fits
 
 # Open the FITS file
-hdul = fits.open('hlsp_jwst-ero_jwst_miri_stephansquintet_f1000w_v1_i2d.fits')
+hdul = fits.open('hlsp_jwst-ero_jwst_miri_stephansquintet_f770w_v1_i2d.fits')
 
 # Get the pixel data
-data = hdul[0].data
+data = hdul[1].data
 
 # Print the shape of the data
 print("Data shape:", data.shape)
 
 # Set the numpy print options
-np.set_printoptions(threshold=np.inf, precision=4, suppress=True)
+np.set_printoptions(threshold=np.inf, precision=10, suppress=True)
 
 # Set the number of rows to print
-num_rows = 1
-num_columns = 1
+num_rows = 400
+num_columns = 400
 
 # Print the data
 print("Data values:")
